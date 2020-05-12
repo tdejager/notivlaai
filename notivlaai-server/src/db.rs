bb8131ea4e52788f8a99e0548394bf99e1b616ec
Tim de Jager <tdejager89@gmail.com>
2020-05-12 15:56:06 +0200

    feat: seeding and testing for backend

---
+++ notivlaai-server/src/db.rs
@@ -0,0 +1,111 @@
+use crate::schema::*;
+use diesel::*;
+use diesel::{Connection, SqliteConnection};
+
+#[derive(Associations, Identifiable, Queryable)]
+#[table_name = "customer"]
+pub struct Customer {
+    pub id: i32,
+    pub first_name: String,
+    pub last_name: String,
+    pub email: String,
+}
+
+#[derive(Associations, Identifiable, Queryable)]
+#[table_name = "vlaai"]
+pub struct Vlaai {
+    pub id: i32,
+    pub name: String,
+}
+
+#[derive(Associations, Identifiable, Queryable)]
+#[belongs_to(Customer)]
+#[table_name = "order"]
+pub struct Order {
+    pub id: i32,
+    pub customer_id: i32,
+}
+
+#[derive(Associations, Identifiable, Queryable)]
+#[belongs_to(Order)]
+#[table_name = "vlaai_to_order"]
+pub struct VlaaiToOrder {
+    pub id: i32,
+    pub order_id: i32,
+    pub vlaai_id: i32,
+    pub amount: i32,
+}
+
+#[derive(Insertable)]
+#[table_name = "customer"]
+pub struct NewCustomer<'a> {
+    pub first_name: &'a str,
+    pub last_name: &'a str,
+    pub email: &'a str,
+}
+
+#[derive(Insertable)]
+#[table_name = "vlaai"]
+pub struct NewVlaai<'a> {
+    pub name: &'a str,
+}
+
+#[derive(Insertable)]
+#[table_name = "order"]
+pub struct NewOrder {
+    pub customer_id: i32,
+}
+
+#[derive(Insertable)]
+#[table_name = "vlaai_to_order"]
+pub struct NewVlaaiToOrder {
+    pub order_id: i32,
+    pub vlaai_id: i32,
+    pub amount: i32,
+}
+
+pub fn establish_connection() -> SqliteConnection {
+    dotenv::dotenv().ok();
+
+    let database_url = dotenv::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    SqliteConnection::establish(&database_url)
+        .expect(&format!("Error connecting to {}", database_url))
+}
+
+pub fn customer_with_name<T: AsRef<str>>(
+    conn: &SqliteConnection,
+    name: T,
+) -> Result<Vec<Customer>, Box<dyn std::error::Error>> {
+    Ok(customer::table
+        .filter(customer::first_name.like(name.as_ref()))
+        .load(conn)?)
+}
+
+pub fn orders_for_customer(
+    conn: &SqliteConnection,
+    id: i32,
+) -> Result<Vec<Order>, Box<dyn std::error::Error>> {
+    // Find the customer
+    let customer: Customer = customer::table.find(id).get_result(conn)?;
+    Ok(Order::belonging_to(&customer).load(conn)?)
+}
+
+#[cfg(test)]
+mod test {
+
+    #[test]
+    pub fn get_client_with_name() {
+        let conn = super::establish_connection();
+        let results =
+            super::customer_with_name(&conn, "piet").expect("Could not find customer with name");
+        assert!(results.len() > 0)
+    }
+
+    #[test]
+    pub fn order_for_customer() {
+        let conn = super::establish_connection();
+        let results = super::orders_for_customer(&conn, 1)
+            .expect("Could not find orders for customer with this id");
+        assert!(results.len() > 0)
+    }
+}
---
+++ notivlaai-server/src/seed.rs
@@ -0,0 +1,88 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+use db::{NewCustomer, NewOrder, NewVlaai, NewVlaaiToOrder};
+use diesel::prelude::*;
+use diesel::{QueryDsl, RunQueryDsl, SqliteConnection};
+
+#[macro_use]
+extern crate diesel;
+
+mod db;
+mod schema;
+
+/// Insert a vlaai into the database
+fn insert_vlaai(conn: &SqliteConnection, name: &str) {
+    let vlaai = NewVlaai { name };
+    diesel::insert_into(schema::vlaai::table)
+        .values(vlaai)
+        .execute(conn)
+        .expect("Could not insert vlaai");
+}
+
+/// Insert a customer
+fn insert_customer(conn: &SqliteConnection, first_name: &str, last_name: &str, email: &str) {
+    let customer = NewCustomer {
+        first_name,
+        last_name,
+        email,
+    };
+    diesel::insert_into(schema::customer::table)
+        .values(customer)
+        .execute(conn)
+        .expect("Could not insert vlaai");
+}
+
+/// Insert an order
+fn insert_order(conn: &SqliteConnection, name: &str, vlaaien: &[&str]) {
+    let client = schema::customer::table
+        .filter(schema::customer::first_name.eq(name))
+        .first::<db::Customer>(conn)
+        .expect("Could not find customer");
+
+    diesel::insert_into(schema::order::table)
+        .values(NewOrder {
+            customer_id: client.id,
+        })
+        .execute(conn)
+        .expect("Could not insert order");
+
+    let order_id: i32 = schema::order::table
+        .filter(schema::order::customer_id.eq(client.id))
+        .select(schema::order::id)
+        .first(conn)
+        .expect("Could not find order");
+
+    for vlaai in vlaaien {
+        let vlaai_id: i32 = schema::vlaai::table
+            .filter(schema::vlaai::name.eq(vlaai))
+            .select(schema::vlaai::id)
+            .first(conn)
+            .expect("Could not get vlaai");
+
+        diesel::insert_into(schema::vlaai_to_order::table)
+            .values(NewVlaaiToOrder {
+                order_id,
+                vlaai_id,
+                amount: 1,
+            })
+            .execute(conn)
+            .expect("Could not insert vlaai -> order");
+    }
+}
+
+fn main() {
+    let conn = db::establish_connection();
+
+    // Insert vlaaien
+    insert_vlaai(&conn, "Abrikoos");
+    insert_vlaai(&conn, "HalfHalf");
+    insert_vlaai(&conn, "Kers");
+    insert_vlaai(&conn, "Appel");
+    insert_vlaai(&conn, "Kruimelpudding");
+
+    // Insert some customers
+    insert_customer(&conn, "Peter", "Bergmans", "peter@peter.nl");
+    insert_customer(&conn, "Piet", "Pokerface", "pokeren@pokerface.nl");
+
+    insert_order(&conn, "Peter", &vec!["Abrikoos", "Kers"]);
+    insert_order(&conn, "Piet", &vec!["Abrikoos", "Kers"]);
+}
