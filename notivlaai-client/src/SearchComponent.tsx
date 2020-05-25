deeaeba6efcf11f9992d3a24ef24805fed3de2d9
Tim de Jager <tdejager89@gmail.com>
2020-05-25 21:13:20 +0200

    wip: started with search-component

---
+++ notivlaai-client/src/SearchComponent.tsx
@@ -0,0 +1,6 @@
+import { RouteComponentProps } from '@reach/router';
+import React from 'react';
+
+export default function SearchComponent(props: RouteComponentProps) {
+  return <div>Should contain the search component </div>;
+}
